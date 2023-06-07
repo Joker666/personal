c9aebdc25d22a2ebd6e9f4c259cd4fae78c6e417
Hasan <mr.k779@outlook.com>
2023-06-07 21:32:58 +0600

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -72,12 +72,8 @@ export default function About() {
                 myself to type on.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister&apos;s motorcycle helmet,
-                counted down from three, and jumped — hoping the tree was tall
-                enough that with just a bit of momentum I&apos;d be able to get
-                to orbit.
+                I&apos;ve got my job when I was only 20 years old while still at
+                school.
               </p>
               <p>
                 I spent the next few summers indoors working on a rocket design,
